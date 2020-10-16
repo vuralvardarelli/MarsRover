8a3595bb605c97df0e61dc89d65c1721cd3afeb1
Vural Vardarelli <vuralvardarelli@hotmail.com>
2020-10-16 06:51:17 +0300

    IConsole readded

---
+++ MarsRover/Log/ConsoleActions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Log
+{
+    public class ConsoleActions : IConsole
+    {
+        public void ClearConsole()
+        {
+            Console.Clear();
+        }
+
+        public void ExitConsole()
+        {
+            Environment.Exit(0);
+        }
+
+        public ConsoleKeyInfo ReadKey()
+        {
+            return Console.ReadKey();
+        }
+
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void WritelineToConsole(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ MarsRover/Log/IConsole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Log
+{
+    public interface IConsole
+    {
+        void WritelineToConsole(string text);
+
+        ConsoleKeyInfo ReadKey();
+
+        string ReadLine();
+
+        void ClearConsole();
+
+        void ExitConsole();
+    }
+}
